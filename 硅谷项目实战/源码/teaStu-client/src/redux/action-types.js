4a74af5f57c9ce96a28f39b1c5aa337cef3f6a23
liangwangshou <1275955764@qq.com>
2020-11-18 21:18:44 +0800

    react

--- 硅谷项目实战/源码/teaStu-client/src/redux/action-types.js
+++
@@ -1,8 +0,0 @@
-export const AUTH_SUCCESS = 'auth_success'
-export const ERROR_MSG = 'error_msg'
-export const RECEIVE_USER = 'receive_user'
-export const RESET_USER = 'reset_user'
-export const RECEIVE_USERLIST = 'receive_userlist'
-export const RECEIVE_MSGLIST = 'receive_msglist'
-export const RECEIVEA_MSG = 'receive_msg' //实时接受一条消息
-export const MSG_READ = 'msg_read'
--- 硅谷项目实战/源码/teaStu-client/src/utils/index.js
+++
@@ -1,9 +0,0 @@
-export function getRedirectPath(type, header) {
-    let path = ''
-    path += type === 'teacher' ? '/teacher' : '/student'
-    if (!header) {
-        path += 'info'
-    }
-    return path
-
-}
